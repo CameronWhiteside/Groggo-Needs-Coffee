7934297c5eeb944be6cf871d203c1e1987493ab3
Cameron Whiteside <whiteside.cameron@gmail.com>
2022-02-24 19:33:36 -0700

    corrected site description on visualizer page

--- react-app/src/components/Lorem/Lorem.js
+++ react-app/src/components/Lorem/Lorem.js
@@ -1,8 +1,8 @@
 const Lorem = ({ chars }) => {
     const text = `
     Groggo needs coffee as badly as Cameron Whiteside needs a job, and only you can fix that!
-     Each map begins as a weighted graph of 2,450 nodes represented a 70 x 35 grid. Each pair of orthogonally and diagonally adjacent nodes are joined by an edge with
-     a weight equal to the nodes' distance on screen. You can modify edge weights by adding, editing, and removing features. When finding a path,
+     Each map begins as a weighted graph of 2,450 nodes represented by a 70 x 35 grid. Each pair of orthogonally and diagonally adjacent nodes are joined by an edge with
+     a weight equal to the nodes' Euclidian distance. You can modify edge weights by adding, editing, and removing features. When finding a path,
      the application generates an adjacency list based on your unique map and animates the node visitation order and shortest path to the coffee shop according
         to Dijkstra's algorithm.
     `
